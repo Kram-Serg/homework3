0bfb73b76b77649563762dd6eccd093f50d76d2f
Kram-Serg <bild003@yandex.ru>
2024-07-17 13:11:01 +0700

    Update homework3.py

--- homework3.py
+++ homework3.py
@@ -2,7 +2,7 @@ name = "Сергей"
 print(f'Name:',name)
 age = 38
 print(f"Age: {age}")
-new_age = age+1
-print(f"New Age: {new_age}")
+age = age+1
+print(f"New Age: {age}")
 is_student = 'True'
 print(f"Is Student: {is_student}")
